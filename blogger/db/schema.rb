44e1b608b8b6400b49b3fba973d6ce27dd1fb087
orlando21 <me@gregbabb.com>
2018-03-01 20:09:07 +0100

    Completed tutorial I4

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228163346) do
+ActiveRecord::Schema.define(version: 20180301181428) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "attachments", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
